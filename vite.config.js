98c1eb81691f49d6c31629c114679f52799e4eef
ShahdElmeniawy <shahedmohamed180@gmail.com>
2026-05-14 23:32:38 +0300

    fix Router

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
+  base: '/badilni-frontend/',
   test: {
     globals: true,
     environment: "jsdom",
