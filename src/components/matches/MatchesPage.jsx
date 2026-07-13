671523a075f0962d043780a5aa22bd408852b9fd
ShahdElmeniawy <shahedmohamed180@gmail.com>
2026-07-13 04:47:26 +0300

    fix size of matches pagination

--- src/components/matches/MatchesPage.jsx
+++ src/components/matches/MatchesPage.jsx
@@ -5,7 +5,7 @@ import MatchCardSkeleton from '../../components/matches/MatchCardSkeleton'
 import ErrorState from '../shared/ErrorState'
 import EmptyState from '../shared/EmptyState'
 
-const PAGE_SIZE = 10
+const PAGE_SIZE = 9
 
 export default function MatchesPage() {
   const [page, setPage] = useState(1)
